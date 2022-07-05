e4b87a267dc289e3328bfd3602a331bdc1148532
SARAI_PECH <spech@blazar.com.mx>
2022-07-05 13:19:16 -0500

    practica 2, singUp

--- widgetgallery/lib/routes/app_routes.dart
+++ widgetgallery/lib/routes/app_routes.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:widgetgallery/models/Item_menu.dart';
 import 'package:widgetgallery/screens/home_screen.dart';
+import '../models/Item_menu.dart';
+import '../screens/login_screen.dart';
+import '../screens/orders_screen.dart';
+import '../screens/profile_screen.dart';
+import '../screens/singup_screen.dart';
+
 
 class AppRoute{
   static const String root = '/';
   static final List<MenuOption> listSreens =[
     MenuOption(title: 'House', route: '/', screen: const HomeScreen(), icon: Icons.house_outlined),
-    MenuOption(title: 'Orders', route: '/orders', screen: const HomeScreen(), icon: Icons.list_outlined),
-    MenuOption(title: 'Login', route: '/login', screen: const HomeScreen(), icon: Icons.login_outlined),
-    MenuOption(title: 'Profile', route: '/profile', screen: const HomeScreen(), icon: Icons.person_outlined),
-    MenuOption(title: 'Sing Up', route: '/singup', screen: const HomeScreen(), icon: Icons.account_circle_outlined)
+    MenuOption(title: 'Orders', route: '/orders', screen: const OrderScreen(), icon: Icons.list_outlined),
+    MenuOption(title: 'Login', route: '/login', screen: const LoginScreen(), icon: Icons.login_outlined),
+    MenuOption(title: 'Profile', route: '/profile', screen: const ProfileScreen(), icon: Icons.person_outlined),
+    MenuOption(title: 'Sing Up', route: '/singup', screen: const SingUpScreen(), icon: Icons.account_circle_outlined)
   ];
 
   static Map<String, Widget Function(BuildContext)> getRoutes() {
